2b6d3fcdc6be80d215bfe8d243ebaf13328283f7
Brian L. Gorman <blgorman@gmail.com>
2026-02-25 11:01:03 -0600

    sso is not quite working

--- SimpleMedicalOfficeWeb/Program.cs
+++ SimpleMedicalOfficeWeb/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using SimpleMedicalOfficeWeb.Data;
 
 namespace SimpleMedicalOfficeWeb;
@@ -10,9 +12,6 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        var microsoftClientID = builder.Configuration["Authentication:Microsoft:ClientId"] ?? throw new InvalidOperationException("Client ID must be set in configuration");
-        var microsoftClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"] ?? throw new InvalidOperationException("Client Secret must be set in configuration");    
-
         // Add services to the container.
         var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -22,16 +21,14 @@ public class Program
         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddRoles<IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>();
-        builder.Services.AddControllersWithViews();
+        builder.Services.AddControllersWithViews()
+            .AddMicrosoftIdentityUI();
 
         builder.Services.AddScoped<IUserRolesService, UserRolesService>();
 
         //add microsoft authorization
-        builder.Services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
-        {
-            microsoftOptions.ClientId = microsoftClientID;
-            microsoftOptions.ClientSecret = microsoftClientSecret;
-        });
+        builder.Services.AddAuthentication()
+            .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
 
         var app = builder.Build();
 
